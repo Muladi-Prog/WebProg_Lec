55efed100baf2ca0676d4c56ad4c41cb70195962
Muladi-Prog <muladibakti@gmail.com>
2022-01-12 22:58:11 +0700

    CreatePost

--- app/Http/Controllers/CreatePostController.php
+++ app/Http/Controllers/CreatePostController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\courses;
+use App\Models\posts;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -13,5 +14,19 @@ class CreatePostController extends Controller
         $courses = courses::all();
 
         return view('createPost', compact('user', 'courses'));
+    }
+    public function create(Request $request,$user_id){
+        $data = $request->validate([
+            'users_id',
+            'title' => 'required|min:5',
+            'description'=>'required|min:10',
+            'courses_id'
+        ]);
+        $data['courses_id'] = $request->courses_id;
+        $data['users_id'] = $user_id;
+        
+        posts::create($data);
+        
+        return redirect()->back()->with('success',"Post Created");
     }
 }
